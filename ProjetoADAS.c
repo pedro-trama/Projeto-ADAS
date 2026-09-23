50da7976d14d32552c7500b683485be092e2d59f
pedro-trama <pedrohenriquebtrama08@gmail.com>
2026-09-23 17:11:02 +0000

    Arquivo criado

---
+++ ProjetoADAS.c
@@ -0,0 +1,13 @@
+/*
+Integrantes do grupo:
+Lucas Notargiacomo Mustaro - RA: 10434914
+Pedro Henrique Bettega Trama - RA: 10769933
+
+Turma: 02D11
+*/
+
+#include <stdio.h>
+
+int main(){
+    return 0;
+}
